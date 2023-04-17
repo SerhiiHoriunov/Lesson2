139815c34e87a3a73abdc62865e82dea60f841fd
SerhiiHoriunov <serghory@gmail.com>
2023-04-17 20:02:02 +0200

    add new folder

---
+++ HomeWork_S_Horiunov/js/myscript.js
@@ -0,0 +1,95 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+// let num;
+// num = 0;
+// num = 1;
+// num = -3;
+
+// if (num > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// num > 0 ? console.log(true) : console.log(false);
+// alert(num);
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// let word;
+// word = 'test';
+// word = "qwerty";
+// word = true;
+
+// word === 'test' ? console.log(true) : console.log(false);
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// let num;
+// // num = 1;
+// num = 10;
+// // num = 13;
+// if (num > 10) {
+//     console.log(num - 5);
+// } else {
+//     console.log(num + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const num = prompt('Введіть число від 1 до 12')
+
+// switch (Number(num)) {
+//     case 1 :
+//         alert('січень');
+//         break;
+//     case 2 :
+//         alert('лютий');
+//         break;
+//     case 3 :
+//         alert('березень');
+//         break;
+//     case 4 :
+//         alert('квітень');
+//         break;
+//     case 5 :
+//         alert('травень');
+//         break;
+//     case 6 :
+//         alert('червень');
+//         break;
+//     case 7 :
+//         alert('липень');
+//         break;
+//     case 8 :
+//         alert('серпень');
+//         break;
+//     case 9 :
+//         alert('вересень');
+//         break;
+//     case 10 :
+//         alert('жовтень');
+//         break;case 1 :
+//         alert('січень');
+//         break;
+//     case 11 :
+//         alert('листопад');
+//         break;
+//     case 12 :
+//         alert('грудень');
+//         break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// let num1 = prompt('Введіть цифру від 1 до 9');
+// let num2 = prompt('Введіть другу цифру від 1 до 9');
+// let num3 = prompt('Введіть третю цифру від 1 до 9');
+// let sum = num1 + num2 + num3;
+// let sum1 = Number(num1) + Number(num2) + Number(num3);
+// alert(`Ви ввели число: ${ sum }`); alert(`Сума цифр цього числа дорівнює:  ${sum1}`);
